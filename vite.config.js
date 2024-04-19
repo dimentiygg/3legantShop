0e6c3dda9ba20100d57175b5966088e865fd7f6e
Dmytro <az.36419.a@gmail.com>
2024-04-19 15:49:04 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  // Остальные настройки...
+  root: resolve(__dirname, 'src/html'),
+});
